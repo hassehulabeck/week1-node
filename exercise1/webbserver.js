f023691204a3cebd651458d9ab9b044f47e5bbe0
hassehulabeck <hansbudandersson@gmail.com>
2020-02-03 11:24:49 +0100

    Fixed host-error

--- exercise1/webbserver.js
+++ exercise1/webbserver.js
@@ -4,7 +4,7 @@ http.createServer(showContent).listen(8080)
 http.createServer(showContent).listen(8081)
 
 function showContent(req, res) {
-    let hostAndPort = req.headers.url
+    let hostAndPort = req.headers.host
     res.write("Hej " + hostAndPort)
     res.end(() => {
         console.log(req.url)
