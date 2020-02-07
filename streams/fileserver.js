5492bab760dc53af852586a39c2ab79ec4ea7771
hassehulabeck <hansbudandersson@gmail.com>
2020-02-07 11:43:35 +0100

    Added error event in streams

--- streams/fileserver.js
+++ streams/fileserver.js
@@ -1,15 +1,23 @@
 const http = require('http')
 const fs = require('fs')
+const eventEmitter = require('events')
 
 
+const errorEmitter = new eventEmitter()
 
 // Vid request, servera filen.
 http.createServer((req, res) => {
     if (req.url == "/") {
         fs.readFile('./bigfile.txt', (err, data) => {
-            if (err) console.error(err)
+            if (err) {
+                errorEmitter.emit('fileNotExist', 'bigfile.txt')
+            }
 
             res.end(data)
         })
     }
 }).listen(8080)
+
+errorEmitter.on('fileNotExist', (filename) => {
+    console.log(filename + " not exist")
+})
